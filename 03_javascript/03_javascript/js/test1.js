0ca12e0e9ed19f09fbd2c9dbd5a471db4986aa5d
깔끔한청년 <62824389+rlwi440@users.noreply.github.com>
2021-03-03 22:50:51 +0900

    Delete 03_javascript/03_javascript directory

--- 03_javascript/03_javascript/js/test1.js
+++
@@ -1,9 +0,0 @@
-function check(chk) {
-    var test = document.getElementsByName("product");
-    for (var i = 0; i < test.length; i++) {
-        //    if (test[i] != chk) {  test[i]  가 != chk 가 아닌경우  옮기는방식??
-        if (test[i] != chk) {
-            test[i].checked = false;
-        }
-    }
-}
